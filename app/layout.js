6e34c9eaf7928774285586babb1f55e1116c0713
ayush-i2e <ayush.jhota@i2econsulting.com>
2025-03-24 13:08:18 +0530

    Implement flexbox layout to fix footer positioning

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div style={{flexGrow: 1}}>
+          {children}
+        </div>
       </body>
     </html>
   );
